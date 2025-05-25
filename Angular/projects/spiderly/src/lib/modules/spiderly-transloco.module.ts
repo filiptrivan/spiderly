7bc1bc9cbb9a9ce29417b3864a2c8be7052cb584
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-05-26 00:44:41 +0200

    v19.0.0 update.

--- Angular/projects/spiderly/src/lib/modules/spiderly-transloco.module.ts
+++ Angular/projects/spiderly/src/lib/modules/spiderly-transloco.module.ts
@@ -23,14 +23,6 @@ export class SpiderlyTranslocoModule {
             ],
             defaultLang: config?.defaultLang ?? 'en',
             fallbackLang: config?.fallbackLang ?? 'en.generated',
-            // availableLangs: [
-            //   'sr-Latn-RS', 'sr-Latn-RS.generated', 
-            //   'en', 'en.generated',
-            // ],
-            // defaultLang: 'sr-Latn-RS',
-            // fallbackLang: [
-            //   'sr-Latn-RS.generated',
-            // ],
             missingHandler: {
               useFallbackTranslation: true,
               logMissingKey: false,
