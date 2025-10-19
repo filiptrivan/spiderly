710b3c8411224f2c7c6573bbbe05fd8107e4ab88
filiptrivan <120225885+filiptrivan@users.noreply.github.com>
2025-10-19 20:23:58 +0200

    ReadonlyObject.cs update, deleted protected setter

--- Spiderly.Shared/BaseEntities/ReadonlyObject.cs
+++ Spiderly.Shared/BaseEntities/ReadonlyObject.cs
@@ -1,20 +1,14 @@
 ﻿using Spiderly.Shared.Interfaces;
-using System;
-using System.Collections.Generic;
-using System.ComponentModel.DataAnnotations;
-using System.ComponentModel.DataAnnotations.Schema;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
 
 namespace Spiderly.Shared.BaseEntities
 {
     /// <summary>
     /// If CRUD operations can be performed on the entity from the application, it should inherit BusinessObject&lt;ID&gt;, if the entity is only for reading from the database (e.g. Gender entity), it should inherit ReadonlyObject&lt;ID&gt;. For BusinessObject entities, the necessary methods for basic CRUD operations will be generated, while e.g. for ReadonlyObject entities Create, Update, Delete methods will not be generated. For ReadonlyObject&lt;T&gt; we don't make CreatedAt and Version properties.
+    /// Id is not protected here because in most cases we want to assign it inside EF Core infrastructure project manually.
     /// </summary>
     /// <typeparam name="T">Entity's Id type (long/int/byte)</typeparam>
     public class ReadonlyObject<T> : IReadonlyObject<T> where T : struct
     {
-        public T Id { get; protected set; }
+        public T Id { get; set; }
     }
 }
